d09951a74eaf89d8cc59c0ab05a2ec366740cab1
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-11 16:13:15 +0200

    improved struct & added one for addresses

--- TW-CDXX/components/tee.h
+++ TW-CDXX/components/tee.h
@@ -2,13 +2,37 @@
 
 class Tee
 {
+private:
 	struct InputData
 	{
-		int m_Direction;
-		int m_Hook;
-		int m_Fire;
+		int m_TargetX;
+		int m_TargetY;
 		int m_Jump;
+		int m_Fire;
+		int m_Hook;
+		int m_PlaceHolder1;
+		int m_PlaceHolder2;
+		int m_PlaceHolder3;
+		int m_PlaceHolder4;
 		int m_DirLeft;
 		int m_DirRight;
 	};
+
+	struct InputAddresses
+	{
+		int m_TargetX;
+		int m_TargetY;
+		int m_Jump;
+		int m_Fire;
+		int m_Hook;
+		int m_PlaceHolder1;
+		int m_PlaceHolder2;
+		int m_PlaceHolder3;
+		int m_PlaceHolder4;
+		int m_DirLeft;
+		int m_DirRight;
+	};
+	//DWORD* test = (DWORD*)pController->getPatternScan()->patternScanExModule((char*)"\x00\x00\x00\x00\x00\x97\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00", (char*)"xxxxxx?x????????????xxxxxxxx");
+public:
+	
 };
