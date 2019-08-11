6b9c2c5cc5e31774ee1a369d6cd3fabd0724f035
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-11 21:09:04 +0200

    """""""fixed""""""" complilation

--- TW-CDXX/components/tee.h
+++ TW-CDXX/components/tee.h
@@ -1,6 +1,6 @@
 #pragma once
+
 #include "../controller.h"
-#include "../memory.h"
 
 enum
 {
--- TW-CDXX/main.cpp
+++ TW-CDXX/main.cpp
@@ -2,14 +2,24 @@
 #include "patternscan.h"
 #include "memory.h"
 #include "controller.h"
-
 #include "components/tee.h"
+
+Tee* pTee;
+
+void tick()
+{
+	while (420 == 420)
+	{
+		pTee->tick();
+		std::this_thread::sleep_for(std::chrono::milliseconds(TICK_SPEED));
+	}
+};
 
 int main()
 {
 	Controller* pController = new Controller((wchar_t*)L"teeworlds.exe", (wchar_t*)L"teeworlds.exe");
-	
-	Tee* pTee = new Tee(pController);
+	pTee = new Tee(pController);
+	tick();
 
 	return  0;
 }
